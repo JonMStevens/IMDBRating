acb7f0a00f528a17d0bb56e8c15f0d3d0e29fe0e
Jonathan <jms15af@my.fsu.edu>
2021-05-28 15:16:06 -0400

    Remove the need for a season count param

--- imdbrating2.py
+++ imdbrating2.py
@@ -1,4 +1,5 @@
-# todo elim need for 2nd param
+# speed up
+# could add some brief search feature?
 
 import sys
 import urllib.request
@@ -7,34 +8,37 @@ import re
 
 class IMDBInfoGrabber:
     @staticmethod
-    def GetIMDBInfoForShow(imdbTitleID, seasonCount):
+    def GetIMDBInfoForShow(imdbTitleID):
         if type(imdbTitleID) != type("a string"):
             print("IMDb Code was not type string")
             return ""
-        if type(seasonCount) != type(1):
-            print("Season Count was not type int")
-            return ""
         if imdbTitleID == "":
             print("IMDb Code was empty string")
-            return ""
-        if seasonCount < 0:
-            print("Season Count was less than 1")
             return ""
         if re.fullmatch("^tt\d+$", imdbTitleID) is None:
             print("IMDb Code did not match expected format")
             return ""
 
         print(imdbTitleID)
-        ret = "Season,Episode,Air Date,Title,Rating,Rating Count\n"
-        for i in range(1, seasonCount + 1):
+        print("Working on Season 1")
+
+        csvText = "Season,Episode,Air Date,Title,Rating,Rating Count\n"
+        ret = IMDBInfoGrabber.__getEpisodeInfoForSeason(
+            imdbTitleID, 1)
+        csvText += ret[0]
+        seasonCount = ret[1]
+
+        for i in range(2, seasonCount + 1):
             print("Working on Season " + str(i))
-            ret += IMDBInfoGrabber.__getEpisodeInfoForSeason(
-                imdbTitleID, i) + "\n"
+            ret = IMDBInfoGrabber.__getEpisodeInfoForSeason(
+                imdbTitleID, 1)
+            csvText += ret[0]
 
         print("OK")
-        return ret.strip()
+        return csvText.strip()
 
     def __getEpisodeInfoForSeason(imdbTitleID, seasonNumber):
+        # todo could this be sped up by using find inside the block instead of regex?
         episodeBlockRe = re.compile(
             "<div class=\"info\" itemprop=\"episodes\".*?</div>.*?ipl-rating-star__total-votes.*?</span>", re.DOTALL)
         episodeNumberRe = re.compile("(?<=episodeNumber\" content=\")\d+")
@@ -48,9 +52,9 @@ class IMDBInfoGrabber:
         # todo give better message if url does not work
         with urllib.request.urlopen(f"https://www.imdb.com/title/{imdbTitleID}/episodes?season={seasonNumber}") as response:
             html = str(response.read())
-            episodeInfoBlocks = episodeBlockRe.findall(html)
+            episodeInfoBlocksHTML = episodeBlockRe.findall(html)
 
-            for block in episodeInfoBlocks:
+            for block in episodeInfoBlocksHTML:
                 line = str(seasonNumber) + ","
                 line += episodeNumberRe.search(block).group() + ","
                 line += airDateRe.search(block).group() + ","
@@ -61,24 +65,28 @@ class IMDBInfoGrabber:
                 line += rateCount.search(block).group().replace(",",
                                                                 "").rstrip(")")
                 csvLines.append(line)
-        return "\n".join(csvLines)
+
+            seasonCount = -1
+            if seasonNumber == 1:
+                seasonDropdownHTML = re.search(
+                    "<select id=\"bySeason\".*?</select>", html).group()
+                seasonCount = len(re.findall(
+                    "<option.*?</option>", seasonDropdownHTML))
+
+        return "\n".join(csvLines), seasonCount
 
 
 def __main__():
-    if len(sys.argv) < 4:
+    if len(sys.argv) < 3:
         print("Missing Arguments")
         return
-
+    
+    # todo put more requirement on filenames
+    # todo give better message if this does not work
     try:
-        seasonCount = int(sys.argv[2])
-    except:
-        print("Invalid Season Count: " + sys.argv[2])
-        return
-
-    try:
-        with open(sys.argv[3], "w") as csv:
+        with open(sys.argv[2], "w") as csv:
             csv.write(IMDBInfoGrabber.GetIMDBInfoForShow(
-                sys.argv[1], seasonCount))
+                sys.argv[1]))
     except:
         print(sys.exc_info()[1])
 
