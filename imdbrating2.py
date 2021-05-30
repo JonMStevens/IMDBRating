1f53b922cb89669e933c6f880205ed1b105bb587
Jonathan <jms15af@my.fsu.edu>
2021-05-29 20:37:39 -0400

    Protect against bad parameters/inputs

--- imdbrating2.py
+++ imdbrating2.py
@@ -2,6 +2,7 @@
 # could add some brief search feature?
 # this might be a good opportunity for async. do multiple seasons at once
 
+from os import error
 import sys
 import urllib.request
 import re
@@ -11,14 +12,11 @@ class IMDBInfoGrabber:
     @staticmethod
     def GetIMDBInfoForShow(imdbTitleID):
         if type(imdbTitleID) != type("a string"):
-            print("IMDb Code was not type string")
-            return ""
+            raise TypeError("IMDb Code was not type string")
         if imdbTitleID == "":
-            print("IMDb Code was empty string")
-            return ""
+            raise ValueError("IMDb Code was empty string")
         if re.fullmatch("^tt\d+$", imdbTitleID) is None:
-            print("IMDb Code did not match expected format")
-            return ""
+            raise ValueError("IMDb Code did not match expected format")
 
         print(imdbTitleID)
         print("Working on Season 1")
@@ -52,6 +50,10 @@ class IMDBInfoGrabber:
         csvLines = []
         episodeInfoBlocksHTML = episodeBlockRe.findall(html)
 
+        if (len(episodeInfoBlocksHTML) == 0):
+            raise error(
+                "Could not find episodes for this show. This script may need fixing")
+
         for block in episodeInfoBlocksHTML:
             line = str(seasonNumber) + ","
             line += episodeNumberRe.search(block).group() + ","
@@ -67,20 +69,38 @@ class IMDBInfoGrabber:
         return "\n".join(csvLines) + "\n"
 
     def __getSeasonHTML(imdbTitleID, seasonNumber):
-        # todo give better message if url does not work
-        with urllib.request.urlopen(f"https://www.imdb.com/title/{imdbTitleID}/episodes?season={seasonNumber}") as response:
-            return str(response.read())
+        try:
+            with urllib.request.urlopen(f"https://www.imdb.com/title/{imdbTitleID}/episodes?season={seasonNumber}") as response:
+                return str(response.read())
+        except:
+            raise ValueError(
+                "A season page could not be found using the given IMDb Title ID")
+
     def __getSeasonCount(html):
-        seasonDropdownHTML = re.search("<select id=\"bySeason\".*?</select>", html).group()
+        try:
+            seasonDropdownHTML = re.search(
+                "<select id=\"bySeason\".*?</select>", html).group()
+        except:
+            raise error(
+                "Could not find number of seasons in given HTML. This script may need fixing")
+
         return len(re.findall("<option.*?</option>", seasonDropdownHTML))
 
+
 def __main__():
-    if len(sys.argv) < 3:
+    if len(sys.argv) != 3:
         print("Missing Arguments")
         return
-    
-    # todo put more requirement on filenames
-    # todo give better message if this does not work
+    if type(sys.argv[2]) != type("a string"):
+        print("CSV file name parameter was not a string")
+        return
+    if sys.argv[2] == "":
+        print("CSV file name parameter was empty string")
+        return
+    if re.match("^[\w,\s-]+\.csv$", sys.argv[2]) is None:
+        print("CSV file name parameter not in the correct format. It should be something like example.csv")
+        return
+
     try:
         with open(sys.argv[2], "w") as csv:
             csv.write(IMDBInfoGrabber.GetIMDBInfoForShow(
