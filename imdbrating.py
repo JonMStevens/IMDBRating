2e62281449fd54b5bbbfd79d88ce20b10a0d5858
Jonathan <jms15af@my.fsu.edu>
2022-04-06 21:30:02 -0400

    add unit tests for parse_args

--- imdbrating.py
+++ imdbrating.py
@@ -3,10 +3,12 @@ Call static method get_imdb_info_for_show,
 pass in an imdb code (found in url) and write returned string to file
 """
 #todo: add types to functions
+#todo: move unit tests
 # speed up
 # could add some brief search feature?
 # this might be a good opportunity for async. do multiple seasons at once
 
+from io import TextIOWrapper
 from os import error
 import sys
 import urllib.request
@@ -32,7 +34,7 @@ class TestIMDBInfoGrabber(unittest.TestCase):
         """test imdb code of episode
         imdb code for futurama: luck of the fryish"""
         with self.assertRaises(ValueError):
-                IMDBInfoGrabber.get_imdb_info_for_show("tt0768678")
+            IMDBInfoGrabber.get_imdb_info_for_show("tt0768678")
     def test_nothing_code(self):
         """test valid imdb code that points to nothing"""
         with self.assertRaises(ValueError):
@@ -65,6 +67,157 @@ class TestIMDBInfoGrabber(unittest.TestCase):
         """test a float that is not a code"""
         with self.assertRaises(ValueError):
             IMDBInfoGrabber.get_imdb_info_for_show(1.2)
+class TestParseArgs(unittest.TestCase):
+    """tests parse_args"""
+    @staticmethod
+    def has_all_attr(args: argparse.Namespace) -> bool:
+        """helper function to test that all args exist in namespace"""
+        return hasattr(args, "url") and hasattr(args, "code") and hasattr (args, "csv_file")
+    def test_happy_with_code(self):
+        """happy path test with code as arg
+        imdb code for Home Movies"""
+        argv = ["-code", "tt0197159", "homemovies.csv"]
+        args = parse_args(argv)
+        self.assertIsInstance(args, argparse.Namespace)
+        self.assertTrue(TestParseArgs.has_all_attr(args))
+        self.assertIsNone(args.url)
+        self.assertEqual(args.code, ["tt0197159"])
+        self.assertIsInstance(args.csv_file, TextIOWrapper)
+        args.csv_file.close()
+
+    def test_happy_with_url_episode_list(self):
+        """happy path test with url as arg
+        url goes to episode list
+        code is for star trek deep space 9"""
+        argv = ["-url", "https://www.imdb.com/title/tt0106145/episodes?season=1", "ds9.csv"]
+        args = parse_args(argv)
+        self.assertIsInstance(args, argparse.Namespace)
+        self.assertTrue(TestParseArgs.has_all_attr(args))
+        self.assertIsNone(args.code)
+        self.assertEqual(args.url, ["tt0106145"])
+        self.assertIsInstance(args.csv_file, TextIOWrapper)
+        args.csv_file.close()
+    def test_happy_with_url(self):
+        """happy path test with url as arg
+        code is for star trek deep space 9"""
+        argv = ["-url", "https://www.imdb.com/title/tt0106145/", "ds9.csv"]
+        args = parse_args(argv)
+        self.assertIsInstance(args, argparse.Namespace)
+        self.assertTrue(TestParseArgs.has_all_attr(args))
+        self.assertIsNone(args.code)
+        self.assertEqual(args.url, ["tt0106145"])
+        self.assertIsInstance(args.csv_file, TextIOWrapper)
+        args.csv_file.close()
+    def test_happy_with_url_no_trailing_slash(self):
+        """happy path test with url as arg
+        url has no traling /
+        code is for star trek deep space 9"""
+        argv = ["-url", "https://www.imdb.com/title/tt0106145", "ds9.csv"]
+        args = parse_args(argv)
+        self.assertIsInstance(args, argparse.Namespace)
+        self.assertTrue(TestParseArgs.has_all_attr(args))
+        self.assertIsNone(args.code)
+        self.assertEqual(args.url, ["tt0106145"])
+        self.assertIsInstance(args.csv_file, TextIOWrapper)
+        args.csv_file.close()
+    def test_happy_with_url_with_ref(self):
+        """happy path test with url as arg with a ?ref in url
+        code is for fridays"""
+        argv = ["-url", "https://www.imdb.com/title/tt0080219/?ref_=nm_flmg_wr_14", "fridays.csv"]
+        args = parse_args(argv)
+        self.assertIsInstance(args, argparse.Namespace)
+        self.assertTrue(TestParseArgs.has_all_attr(args))
+        self.assertIsNone(args.code)
+        self.assertEqual(args.url, ["tt0080219"])
+        self.assertIsInstance(args.csv_file, TextIOWrapper)
+        args.csv_file.close()
+    def test_double_dashes(self):
+        """test with double dashes in front of arg flag, should reject"""
+        argv = ["--code", "tt0197159", "homemovies.csv"]
+        with self.assertRaises(SystemExit):
+            parse_args(argv)
+
+    def test_csv_arg_flag(self):
+        """test with arg flag for csv arg, should reject"""
+        argv = ["-code", "tt0197159", "-csv", "homemovies.csv"]
+        with self.assertRaises(SystemExit):
+            parse_args(argv)
+
+    def test_non_imdb_url(self):
+        """test url that is not an imdb site, should reject"""
+        argv = ["-url",
+        ("https://stackoverflow.com/questions/827557/"
+        "how-do-you-validate-a-url-with-a-regular-expression-in-python"),
+        "ds9.csv"]
+        with self.assertRaises(SystemExit):
+            parse_args(argv)
+
+    def test_non_url(self):
+        """test url that goes nowhere, should reject"""
+        argv = ["-url", "https://alfjalafafghagla.zzz/", "ds9.csv"]
+        with self.assertRaises(SystemExit):
+            parse_args(argv)
+
+    def test_only_csv(self):
+        """test argv only has csv argument, missing code or url"""
+        argv = ["homemovies.csv"]
+        with self.assertRaises(SystemExit):
+            parse_args(argv)
+
+    def test_only_code(self):
+        """test argv only has csv argument, missing code or url"""
+        argv = ["tt0197159"]
+        with self.assertRaises(SystemExit):
+            parse_args(argv)
+
+    def test_only_flag(self):
+        """test argv only has csv argument, missing code or url"""
+        argv = ["-code"]
+        with self.assertRaises(SystemExit):
+            parse_args(argv)
+
+    def test_missing_arg_flag(self):
+        """test argv is missing -url or -code flag"""
+        argv = ["tt0197159", "homemovies.csv"]
+        with self.assertRaises(SystemExit):
+            parse_args(argv)
+
+    def test_empty_code(self):
+        """test empty string as code arg"""
+        argv = ["-code", "", "homemovies.csv"]
+        with self.assertRaises(SystemExit):
+            parse_args(argv)
+
+    def test_empty_csv(self):
+        """test empty string as csv arg"""
+        argv = ["-code", "tt0197159", ""]
+        with self.assertRaises(SystemExit):
+            parse_args(argv)
+
+    def test_nameless_csv(self):
+        """test .csv csv arg"""
+        argv = ["-code", "tt0197159", ".csv"]
+        with self.assertRaises(SystemExit):
+            parse_args(argv)
+
+    def test_short_code(self):
+        """test invalid imdb code that is too short"""
+        argv = ["-code", "tt12", ".csv"]
+        with self.assertRaises(SystemExit):
+            parse_args(argv)
+
+    def test_long_code(self):
+        """test invalid imdb code that is too long"""
+        argv = ["-code", "tt122112112211212", ".csv"]
+        with self.assertRaises(SystemExit):
+            parse_args(argv)
+
+    def test_non_code(self):
+        """test a string that is not a code"""
+        argv = ["-code", "t", "homemovies.csv"]
+        with self.assertRaises(SystemExit):
+            parse_args(argv)
+
 class IMDBInfoGrabber:
     """class used for retrieving csv info.
     Main method is get_imdb_info_for_show
